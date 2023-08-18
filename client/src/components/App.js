7efc7a8a898592fd49494a2e7e7b7831ad3acff8
Karla Degrano-Sebandal <karla.degrano@gmail.com>
2023-08-18 10:09:41 +0800

    added some pages

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,27 @@
-import React from "react";
+import React from "react"
 import '../assets/styles/App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 //navbar access
-import NavBar from './NavBar';
-import About from "./About";
-import Contact from "./Contact";
-import Services from "./Services";
+import NavBar from './NavBar'
+import About from "./About"
+import Contact from "./Contact"
+import Services from "./Services"
 
 //main access
-import HomePage from '../pages/HomePage';
-import Booking from './Booking';
-import RopeAccess from '../pages/RopeAccess';
-import WhiteWaterRafting from '../pages/WhiteWaterRafting';
+import HomePage from '../pages/HomePage'
+import Booking from './Booking'
+import RopeAccess from '../pages/RopeAccess'
+import WhiteWaterRafting from '../pages/WhiteWaterRafting'
 
 //footer access
-import Footer from './Footer';
-import UserLogin from "./UserLogin"; //Admin
-import UserRegistration from "./UserRegistration"; //AOI Registration
-import GuestLogin from "./GuestLogin"; //Guest
-import UserDashboard from "./UserDashboard";
+import Footer from './Footer'
+import UserLogin from "./UserLogin" //Admin
+import UserRegistration from "./UserRegistration" //AOI Registration
+import GuestLogin from "./GuestLogin" //Guest
+import UserDashboard from "./UserDashboard"
+import GuestDashboard from "./GuestDashboard"
+import GuestRegistration from "./GuestRegistration"
 
 function App() {
   return (
@@ -36,10 +38,13 @@ function App() {
           <Route path="/services" element={<Services />} />
           <Route path="/booking" element={<Booking />} />
           <Route path="/contact" element={<Contact />} />
+          
           <Route path="/admin" element={<UserLogin />} />
           <Route path="/login" element={<GuestLogin />} />
           <Route path="/uregistration" element={<UserRegistration />} />
           <Route path="/userdashboard" element={<UserDashboard />} />
+          <Route path="/guestregistration" element={<GuestRegistration />} />
+          <Route path="/guestdashboard" element={<GuestDashboard />} />
 
           <Route path="/ropeaccess" element={<RopeAccess />} />
           <Route path="/whitewaterrafting" element={<WhiteWaterRafting />} />
