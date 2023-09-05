02f7bf8eba3068cd497a267a2baca4c6a5cf2924
Karla Degrano-Sebandal <karla.degrano@gmail.com>
2023-09-04 20:16:05 -0600

    booking display only

--- client/src/components/UserDashboard.js
+++ client/src/components/UserDashboard.js
@@ -1,56 +1,94 @@
-import React, { useState, useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
-import axios from 'axios';
+import React, { useState, useEffect, Fragment } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+
+import UserSideBar from "./UserSideBar"
+import UserEditBooking from "./UserEditBooking";
 
 const UserDashboard = () => {
-  const location = useLocation();
-  const { username } = location.state || {};
+  const location = useLocation()
+  const { username } = location.state || {}
+  const [bookings, setBooking] = useState([])
+
+  const deleteBooking = async (bookingid) => {
+    try {
+        const deleteBooking = await fetch(`http://localhost:5000/booking/${bookingid}`, {
+            method: "DELETE"
+        });
+
+        setBooking(bookings.filter(booking => booking.bookingid !== bookingid));
+
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+const getBookings = async() => {
+    try {
+        
+        const response = await fetch("http://localhost:5000/booking")
+        const jsonData = await response.json();
+        
+        setBooking(jsonData);
+
+    } catch (err) {
+        console.error(err.message)
+    }
+}
+
+useEffect(() => {
+  getBookings();
+}, [])
 
   return (
-    <div class="mt-20 flex h-screen w-[250px] flex-col bg-purity pt-10">
-      {username ? (
-        <div class="bg-purity">
-          <ul class="flex w-full flex-col gap-3 px-3">
-            <li>
-              <a href="" class="flex items-center justify-start gap-2 rounded-full bg-slate-800 px-3 py-2.5 text-sm font-medium text-slate-200 ring-offset-2 ring-offset-slate-950 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500">
-              Dashboard</a>
-            </li>
-            <li>
-              <a href="" class="flex items-center justify-start gap-2 rounded-full px-3 py-2.5 text-sm font-medium text-slate-400 ring-offset-2 ring-offset-slate-950 hover:bg-slate-800 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500">
-              Feed</a>
-            </li>
-          </ul>
-          <div class="mt-auto border-t border-slate-800 px-2 py-2">
-            <div class="flex items-center gap-2">
-              <div class="h-8 w-8 overflow-hidden rounded-full">
-                <img class="rounded-full h-full w-full" src="https://picsum.photos/200" />
+    <Fragment class="flex">
+    {" "}
+      <div class="container full-width mt-16">
+        {username ? (
+
+          <div class="flex flex-row">
+            <UserSideBar username={username} />
+              {" "}
+              <div class="relative overflow-x-auto">
+              <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" class="px-6 py-3">Service ID</th>
+                    <th scope="col" class="px-6 py-3">Guest ID</th>
+                    <th scope="col" class="px-6 py-3">Booking Date</th>
+                    <th scope="col" class="px-6 py-3">Booking Time</th>
+                    <th scope="col" class="px-6 py-3">Booking Created</th>
+                    <th scope="col" class="px-6 py-3">Status</th>
+                    <th scope="col" class="px-6 py-3">Booking Type</th>
+
+                  </tr>
+                </thead>
+                <tbody>
+                    {bookings.map(book => (
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" 
+                        key={book.bookingid}>
+                        <td scope="col" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{book.serviceid}</td>
+                        <td scope="col" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{book.guestid}</td>
+                        <td scope="col" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{book.bookingdate}</td>
+                        <td scope="col" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{book.bookingtime}</td>
+                        <td scope="col" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{book.bookingcreated}</td>
+                        <td scope="col" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{book.status}</td>
+                        <td scope="col" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{book.bookingtype}</td>
+                        <td> <UserEditBooking book={book} /> </td>
+                        <td><button class="btn btn-danger" onClick={() => deleteBooking(book.bookingid)}>Delete</button></td>
+                    </tr>
+                
+
+                ))}
+                </tbody>
+              </table>
               </div>
-              <div>
-                <p class="text-sm text-marble-blue">{`${username}`}</p>
-                <p class="text-xs text-slate-400">creator</p>
-              </div>
-              {/* <button x-data="{shown:true}" class="relative ml-auto flex h-6 w-6 items-center justify-center rounded-full hover:bg-slate-800">
-                <div x-show="shown" x-transition class="absolute bottom-7 left-0 z-10 mt-2 w-48 rounded-lg  bg-slate-950 text-left text-sm shadow-lg">
-                  <div class="p-1">
-                    <a href="#" class="flex w-full items-center rounded-md px-3 py-2 text-slate-300 hover:bg-slate-700 hover:text-slate-100"> Dashboard </a>
-                    <a href="#" class="flex w-full items-center rounded-md px-3 py-2 text-left text-sm text-slate-300 hover:bg-slate-700 hover:text-slate-100"> Settings </a>
-                    <a href="#" class="flex w-full items-center rounded-md px-3 py-2 text-left text-sm text-slate-300 hover:bg-slate-700 hover:text-slate-100"> Help </a>
-                    <a href="#" class="flex w-full items-center rounded-md px-3 py-2 text-slate-300 hover:bg-slate-700 hover:text-slate-100"> Log out </a>
-                  </div>
-                </div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-white lucide lucide-more-horizontal" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                  <circle cx="12" cy="12" r="1"></circle>
-                  <circle cx="19" cy="12" r="1"></circle>
-                  <circle cx="5" cy="12" r="1"></circle>
-                </svg>
-              </button> */}
             </div>
-          </div>
-        </div>
-      ) : (
-        <p>Loading user data...</p>
-      )}
-    </div>
+          
+        ) : (
+          <p>Loading user data...</p>
+        )}
+      </div>
+    </Fragment>
   );
 };
 
