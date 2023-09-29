04c6f02d4a99e91fefd9356564fd2d932931de20
Karla Degrano-Sebandal <karla.degrano@gmail.com>
2023-09-29 19:16:31 +0800

    Testing

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -3,7 +3,30 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className="grid grid-cols-1 h-screen w-full mt-20">
-        <div class="mx-auto w-full max-w-[550px]">
+
+<section class="text-gray-600 body-font relative">
+  <div class="absolute inset-0 bg-gray-300">
+    <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.512315360573!2d125.61152927621575!3d7.066440416613454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32f96d61335a5fbf%3A0xc2f8700680f7634f!2sAguila%20Outdoors%20Industry%20-%20AOI!5e0!3m2!1sen!2sph!4v1695352700880!5m2!1sen!2sph"></iframe>
+  </div>
+  <div class="container px-5 py-24 mx-auto flex">
+    <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Contact</h2>
+      <p class="leading-relaxed mb-5 text-gray-600">Leave your message here</p>
+      <div class="relative mb-4">
+        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+      </div>
+      <div class="relative mb-4">
+        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+        <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+      </div>
+      <button class="text-white bg-rescue-orange border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+      <p class="text-xs text-gray-500 mt-3"></p>
+    </div>
+  </div>
+</section>
+
+        {/* <div class="mx-auto w-full max-w-[550px]">
     <form action="https://formbold.com/s/FORM_ID" method="POST">
       <div class="mb-5">
         <label
@@ -73,7 +96,7 @@ const Contact = () => {
         </button>
       </div>
     </form>
-  </div>
+  </div> */}
     </div>
   )
 }
